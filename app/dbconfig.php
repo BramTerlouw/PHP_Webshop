93df9d2ae93cd8c77613eda8e02618a714b4b1d9
marbwoulret <bramterlouw12@gmail.com>
2022-01-02 15:21:13 +0100

    fixed heroku postgress connection

--- app/dbconfig.php
+++ app/dbconfig.php
@@ -1,22 +1,22 @@
 <?php
-// if (getenv('DATABASE_URL') != "") {
-//     $dbopts = parse_url(getenv('DATABASE_URL'));
-//     $type = "pgsql";
-//     $servername = $dbopts["host"];
-//     $username = $dbopts["user"];
-//     $password = $dbopts["pass"];
-//     $database = ltrim($dbopts["path"], '/');
-// }
-// else {
-// $type = "mysql";
+if (getenv('DATABASE_URL') != "") {
+    $dbopts = parse_url(getenv('DATABASE_URL'));
+    $type = "pgsql";
+    $servername = $dbopts["host"];
+    $username = $dbopts["user"];
+    $password = $dbopts["pass"];
+    $database = ltrim($dbopts["path"], '/');
+}
+else {
+    $type = "mysql";
+    $servername = "mysql";
+    $username = "root";
+    $password = "secret123";
+    $database = "developmentdb";
+}
+
 // $servername = "mysql";
 // $username = "root";
 // $password = "secret123";
-// $database = "Webshop";
-// }
-
-$servername = "mysql";
-$username = "root";
-$password = "secret123";
-$database = "developmentdb";
+// $database = "developmentdb";
 ?>
